f35acf73cab2c9526cb253314b9f366362fce0e6
Арман <@ARMAN>
2023-10-25 23:41:15 +0600

    Добавьте файлы проекта.

---
+++ prac8/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class RangeOfArray
+{
+    private int[] data;
+    private int lowerBound; // Нижний индекс допустимого диапазона
+    private int upperBound; // Верхний индекс допустимого диапазона
+
+    public RangeOfArray(int lowerBound, int upperBound)
+    {
+        if (upperBound < lowerBound)
+        {
+            throw new ArgumentException("Верхний индекс должен быть больше или равен нижнему индексу.");
+        }
+
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        int size = upperBound - lowerBound + 1;
+        data = new int[size];
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (IsIndexValid(index))
+            {
+                return data[index - lowerBound];
+            }
+            throw new IndexOutOfRangeException("Индекс находится вне допустимого диапазона.");
+        }
+        set
+        {
+            if (IsIndexValid(index))
+            {
+                data[index - lowerBound] = value;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Индекс находится вне допустимого диапазона.");
+            }
+        }
+    }
+
+    private bool IsIndexValid(int index)
+    {
+        return index >= lowerBound && index <= upperBound;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        RangeOfArray array = new RangeOfArray(6, 10);
+
+        // Установка и получение значений в пользовательском диапазоне
+        array[6] = 10;
+        array[7] = 20;
+        array[8] = 30;
+        array[9] = 40;
+        array[10] = 50;
+
+        for (int i = 6; i <= 10; i++)
+        {
+            Console.WriteLine($"array[{i}] = {array[i]}");
+        }
+    }
+}
